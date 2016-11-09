884d65a647113e706bba123c157f449aaf028e8e
Benjamin Spriggs <benjspriggs@sprico.com>
2016-11-08 17:09:32 -0800

    add setup script

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ FRONTMATTER= \
 #include <cctype>
 using namespace std;
 """
+
 QUESTION_HEADER= \
 """//Place the prototype in the appropriate .h file
 //Consider:
@@ -22,42 +23,43 @@ QUESTION_HEADER= \
 //Remove the block comments form the function below to get started
 """
 
-# With an input tuple,
-# reorder to a proper:
-# { "Q:" question_string, "D:" difficulty, "P:" prompt }
-def get_question_info(input_t):
-    return (input_t[0], input_t[1], input_t[2])
-
-def question_groups(input_s):
-    return VALID_QUESTION.match(input_s)
-
-def valid_question(input_s):
-    return question_groups(input_s) is not None 
-
 # group an iterable into n-ary tuples
 def grouped(iterable, n):
     return zip(*[iter(iterable)]*n)
 
-# Read a file and return a list of tuples:
-# outparam: (question_string, difficulty, prompt)
+def question_groups(input_s):
+    if VALID_QUESTION.match(input_s) is not None:
+        return VALID_QUESTION.match(input_s).groups()
+
+def valid_question(input_s):
+    return question_groups(input_s) is not None 
+
+# With an input tuple,
+# reorder to a proper:
+# { "Q:" question_string, "D:" difficulty, "P:" prompt }
+def get_question_info(input_t):
+    return { question_groups(input_t[x])[0]: \
+            question_groups(input_t[x])[1] \
+            for x in range(3)}
+
+# Read a file and return a dictionary:
+# outparam: { Q: question_string, D: difficulty, P: prompt}
 def read_file(filename):
     valid = map(lambda s: s.rstrip(), filter(valid_question,
         open(filename, "r").readlines()))
     return map(get_question_info, grouped(valid, 3))
 
-# Format a question, given a valid tuple
-def format_question(q_tuple):
-    question = VALID_QUESTION.match(q_tuple[0]).groups()[1]
-    difficulty = VALID_QUESTION.match(q_tuple[1]).groups()[1]
-    prompt = VALID_QUESTION.match(q_tuple[2]).groups()[1]
+# Format a question, given a valid dictionary
+def format_question(q_dict):
     return QUESTION_HEADER \
-            + "//The question is:" + question \
-            + "\n//The difficulty of this question is: " + difficulty \
+            + "//The question is:" + q_dict["Q"]\
+            + "\n//The difficulty of this question is: " + q_dict["D"]\
             + "\n/*\n" \
-            + prompt + "\n{\n\n}" \
+            + q_dict["P"] + "\n{\n\n}" \
             + "\n*/\n"
 
-# From an input filename, format and question
+# From an input filename, format each question
+# and write to file
 def generate_questions(i_filename, o_filename):
     with open(o_filename, "w") as outfile:
         outfile.write(FRONTMATTER)
