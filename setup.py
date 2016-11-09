cd5c9b34d6d55a02eaa531f776ba7e8c896d5c82
Benjamin Spriggs <benjspriggs@sprico.com>
2016-11-08 16:36:09 -0800

    first almost working version

--- setup.py
+++ setup.py
@@ -3,32 +3,38 @@
 # given a formatted question.txt,
 # produce a question.cpp in a proper directory
 
-import re
+import re, argparse
 
-VALID_QUESTION = re.compile("^([D,P,Q]):(.*)")
+VALID_QUESTION = re.compile("^([D,P,Q]): (.*)")
 
 FRONTMATTER= \
 """#include "list.h"
 #include <iostream>
 #include <cstring>
 #include <cctype>
-using namespace std;"""
+using namespace std;
+"""
 QUESTION_HEADER= \
 """//Place the prototype in the appropriate .h file
 //Consider:
 //Do you need helper functions?
 //Is head or tail recursion better?
-//Remove the block comments form the function below to get started"""
+//Remove the block comments form the function below to get started
+"""
 
 # With an input tuple,
 # reorder to a proper:
-# (question_string, difficulty, prompt)
+# { "Q:" question_string, "D:" difficulty, "P:" prompt }
 def get_question_info(input_t):
     return (input_t[0], input_t[1], input_t[2])
 
-def valid_question(input_s):
+def question_groups(input_s):
     return VALID_QUESTION.match(input_s)
 
+def valid_question(input_s):
+    return question_groups(input_s) is not None 
+
+# group an iterable into n-ary tuples
 def grouped(iterable, n):
     return zip(*[iter(iterable)]*n)
 
@@ -36,14 +42,27 @@ def grouped(iterable, n):
 # outparam: (question_string, difficulty, prompt)
 def read_file(filename):
     valid = map(lambda s: s.rstrip(), filter(valid_question,
-                open(filename, "r").readlines()))
+        open(filename, "r").readlines()))
     return map(get_question_info, grouped(valid, 3))
 
-# Print out a question, given a valid tuple
-def question_print(q_tuple):
-    return "//The question is:" + q_tuple[0]
+# Format a question, given a valid tuple
+def format_question(q_tuple):
+    question = VALID_QUESTION.match(q_tuple[0]).groups()[1]
+    difficulty = VALID_QUESTION.match(q_tuple[1]).groups()[1]
+    prompt = VALID_QUESTION.match(q_tuple[2]).groups()[1]
+    return QUESTION_HEADER \
+            + "//The question is:" + question \
+            + "\n//The difficulty of this question is: " + difficulty \
+            + "\n/*\n" \
+            + prompt + "\n{\n\n}" \
+            + "\n*/\n"
+
+# From an input filename, format and question
+def generate_questions(i_filename, o_filename):
+    with open(o_filename, "w") as outfile:
+        outfile.write(FRONTMATTER)
+        for item in map(format_question, read_file(i_filename)):
+            outfile.write(item + "\n")
 
 if __name__ == "__main__":
-    print (FRONTMATTER)
-    print (QUESTION_HEADER)
-    print(list(read_file("LLL/questions.txt")))
+    generate_questions("LLL/questions.txt", "test.cpp")
