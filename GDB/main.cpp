d8f73dd72028a22f11028e62294f09b6d1d51e37
Doug Whitley <dwhitley@pdx.edu>
2017-07-05 13:22:39 -0700

    Correct solutions with comments

--- GDB/main.cpp
+++ GDB/main.cpp
@@ -71,7 +71,7 @@ int readInfo()
         myData.edit(input);
         myData.display();
         // Make sure you pull the appropriate number of characters form the buffer.
-        fIn.get(input,3,':');
+        fIn.get(input,199,':');
         fIn.ignore();
     }
 
